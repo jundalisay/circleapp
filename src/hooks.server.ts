34af51af53ece086220d8cccde4e7138a716965d
“jundalisay” <jundalisay@yahoo.com>
2025-11-26 15:10:24 +0800

    update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,4 +9,17 @@ const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(even
 	});
 });
 
-export const handle: Handle = handleParaglide;
+// export const handle: Handle = handleParaglide;
+
+export const handle: Handle = async ({ event, resolve }) => {
+  if (event.platform?.env) {
+    if (event.platform.env.DATABASE_URL) {
+      process.env.DATABASE_URL = event.platform.env.DATABASE_URL;
+    }
+    if (event.platform.env.DATABASE_AUTH_TOKEN) {
+      process.env.DATABASE_AUTH_TOKEN = event.platform.env.DATABASE_AUTH_TOKEN;
+    }
+  }
+  return resolve(event);
+};
+
